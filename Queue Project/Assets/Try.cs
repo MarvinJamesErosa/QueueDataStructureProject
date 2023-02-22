8c97e2079da53b2370fabcb9a32a424cc92510d0
Kate Anne Marie Dalangin <104249599+kateannemarie@users.noreply.github.com>
2023-02-22 17:29:33 +0800

    feb 22

--- Queue Project/Assets/Try.cs
+++ Queue Project/Assets/Try.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 
 public class Try : MonoBehaviour
 {
-    public GameObject prefab;
-    public float spawnRate = 1f;
-    public float minHeight = -1f;
-    public float maxHeight = 2f;
+    public GameObject[] prefabs;
+    public Transform[] locations;
+    public GameObject[] clones;
 
-    private void OnEnable()
+
+
+
+    public UnityEvent buttonClick;
+
+    void Awake()
     {
-        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+        if (buttonClick == null) { buttonClick = new UnityEvent(); }
     }
 
-    private void OnDisable()
+    public void OnClick()
     {
-        CancelInvoke(nameof(Spawn));
-    }
+        buttonClick.Invoke();
+        //GameObject person = Instantiate(prefab, transform.position, Quaternion.identity);
+        //person.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+        //for (int i = 0; i<1; i++)
+        //{
+        //    Instantiate(prefab, new Vector3(transform.position.x * i, transform.position.y, transform.position.z), Quaternion.identity);
+        //}
+        //Instantiate(prefabs, transform.position, transform.rotation);
 
-    private void Spawn()
-    {
-        GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
-        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+        clones[0] = Instantiate(prefabs[0], locations[0].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        clones[1] = Instantiate(prefabs[1], locations[1].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        clones[2] = Instantiate(prefabs[2], locations[2].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
     }
 
 
